4f05155aefbd273240e579472bb1b92c9ff2b36e
ik7749 <igorkvrnsy@mail.ru>
2023-03-14 21:27:55 +0300

    7 «Асинхронность»

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,57 +1,60 @@
 class AlarmClock {
-  constructor() {
-    this.alarmCollection = [];
-    this.intervalId = null;
-  }
+	constructor() {
+		this.alarmCollection = [];
+		this.intervalId = null;
+	}
 
-  addClock(time, callback) {
-    if (!time || !callback) {
-      throw new Error('Отсутствуют обязательные аргументы');
-    }
+	addClock(time, callback) {
+		if (!time || !callback) {
+			throw new Error('Отсутствуют обязательные аргументы');
+		}
 
-    if (this.alarmCollection.some(element => element.time === time)) {
-      console.warn('Уже присутствует звонок на это же время');
-    }
+		if (this.alarmCollection.some(element => element.time === time)) {
+			console.warn('Уже присутствует звонок на это же время');
+		}
 
-    this.alarmCollection.push(
-      { time, callback, canCall: true }
-    )
-  }
+		this.alarmCollection.push({
+			time,
+			callback,
+			canCall: true
+		})
+	}
 
-  removeClock(time) {
-    this.alarmCollection = this.alarmCollection.filter(element => element.time !== time);
-  }
+	removeClock(time) {
+		this.alarmCollection = this.alarmCollection.filter(element => element.time !== time);
+	}
 
-  getCurrentFormattedTime() { // https://stackoverflow.com/questions/18229022/how-to-show-current-time-in-javascript-in-the-format-hhmmss
-    let time = new Date();
-    String(date.getMinutes()).padStart(2, '0');
-    return (time.getHours() + ':' + time.getMinutes());
-  }
+	getCurrentFormattedTime() {
+		return new Date().toLocaleTimeString("ru-Ru", {
+			hour: "2-digit",
+			minute: "2-digit",
+		});
+	}
 
-  start() {
-    if (this.intervalId !== null) {
-      return;
-    }
+	start() {
+		if (this.intervalId !== null) {
+			return;
+		}
 
-    this.intervalId = setInterval(() => this.alarmCollection.forEach(element => {
-      if (element.time === this.getCurrentFormattedTime()) {
-        element.canCall = false;
-        element.callback();
-      }
-    }), 1000);
-  }
+		this.intervalId = setInterval(() => this.alarmCollection.forEach(element => {
+			if (element.time === this.getCurrentFormattedTime() && element.canCall) {
+				element.canCall = false;
+				element.callback();
+			}
+		}), 1000);
+	}
 
-  stop() {
-    clearInterval(this.intervalId);
-    this.intervalId = null;
-  }
+	stop() {
+		clearInterval(this.intervalId);
+		this.intervalId = null;
+	}
 
-  resetAllCalls() {
-    this.alarmCollection.forEach(element => element.canCall = true);
-  }
+	resetAllCalls() {
+		this.alarmCollection.forEach(element => element.canCall = true);
+	}
 
-  clearAlarms() {
-    this.stop();
-    this.alarmCollection = [];
-  }
+	clearAlarms() {
+		this.stop();
+		this.alarmCollection = [];
+	}
 }
